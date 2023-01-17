2a728d2f581d9f374a7696572a73cc3278c614b9
RexShack <rexshackuk@gmail.com>
2023-01-17 15:57:43 +0000

    more ocd tinkering

--- locales/en.lua
+++ locales/en.lua
@@ -1,28 +1,28 @@
 local Translations = {
     error = {
-          something_went_wrong = 'something went wrong!',
-          dont_have_animal = "don't have an animal on you"
+        something_went_wrong = 'something went wrong!',
+        dont_have_animal = "don't have an animal on you"
     },
     success = {
-          var = 'text goes here',
+        var = 'text goes here',
     },
     primary = {
-          var = 'text goes here',
+        var = 'text goes here',
     },
     menu = {
-          open = 'Open ',
-      sell_your_animal_the_butcher = 'sell your animal to the butcher',
-      buy_items_from_butcher = 'buy items from the butcher',
-      close_menu = 'Close Menu',
-      open_shop = 'Open Shop',
-      sell_animal = 'Sell Animal',
-      butcher_shop = 'Butcher Shop'
+        open = 'Open ',
+        sell_your_animal_the_butcher = 'sell your animal to the butcher',
+        buy_items_from_butcher = 'buy items from the butcher',
+        close_menu = 'Close Menu',
+        open_shop = 'Open Shop',
+        sell_animal = 'Sell Animal',
+        butcher_shop = 'Butcher Shop'
     },
     commands = {
-          var = 'text goes here',
+        var = 'text goes here',
     },
     progressbar = {
-          selling = 'Selling ',
+        selling = 'Selling ',
     },
 }
 
